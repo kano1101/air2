840185f737cd872863d93877967788f17d754f27
Akira Kano <a.kano1101@gmail.com>
2022-01-06 12:37:25 +0900

    ✨item.rsの追加（update関数がまだうまくできていない）

--- src/item.rs
+++ src/item.rs
@@ -13,3 +13,53 @@ pub struct NewItem<'a> {
     pub hash: &'a str,
     pub name: &'a str,
 }
+
+use diesel::prelude::*;
+use diesel::result::Error;
+use diesel::MysqlConnection;
+use transaction::prelude::*;
+use transaction_diesel_mysql::{with_conn, DieselContext};
+
+type Ctx<'a> = DieselContext<'a, MysqlConnection>;
+type BoxTx<'a, T> = Box<dyn Transaction<Ctx = Ctx<'a>, Item = T, Err = Error> + 'a>;
+
+pub fn create<'a>(new: &'a NewItem) -> BoxTx<'a, Item> {
+    use crate::schema::items::table;
+    with_conn(move |cn| {
+        diesel::insert_into(table).values(new).execute(cn)?;
+        table
+            .order(crate::schema::items::id.desc())
+            .limit(1)
+            .first(cn)
+    })
+    .boxed()
+}
+
+pub fn find<'a>(id: i32) -> BoxTx<'a, Option<Item>> {
+    use crate::schema::items::dsl::items;
+    with_conn(move |cn| items.find(id).get_result(cn).optional()).boxed()
+}
+
+pub fn update<'a>(edit: Item) -> BoxTx<'a, Option<()>> {
+    use crate::schema::items::dsl;
+    with_conn(move |cn| {
+        let edit = edit.clone(); // TODO: 本当はclone()したくない
+        diesel::update(dsl::items.find(edit.id))
+            .set((items::hash.eq(edit.hash), items::name.eq(edit.name)))
+            .execute(cn)
+            .map(|_| ())
+            .optional()
+    })
+    .boxed()
+}
+
+pub fn delete<'a>(id: i32) -> BoxTx<'a, Option<()>> {
+    use crate::schema::items::dsl::items;
+    with_conn(move |cn| {
+        diesel::delete(items.find(id))
+            .execute(cn)
+            .map(|_| ())
+            .optional()
+    })
+    .boxed()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate diesel;
+extern crate dotenv;
 extern crate range;
 extern crate rstest;
+extern crate transaction;
 extern crate transaction_diesel_mysql;
 
 mod amazon_browser;
@@ -12,6 +14,20 @@ fn main() {
     println!("Hello, world!");
 }
 
+use diesel::MysqlConnection;
+
+pub fn establish_connection() -> MysqlConnection {
+    use diesel::prelude::*;
+    use dotenv::dotenv;
+    use std::env;
+
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    MysqlConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::*;
@@ -19,5 +35,52 @@ mod tests {
     #[test]
     fn 初期動作確認() {
         assert_eq!(1, 1);
+    }
+    #[test]
+    fn crudの確認() {
+        use super::establish_connection;
+        use crate::item;
+        use crate::item::{Item, NewItem};
+        use crate::transaction::with_ctx;
+        use diesel::result::Error;
+
+        let conn = establish_connection();
+        let tx = with_ctx(|ctx| -> Result<(), Error> {
+            let item = item::create(&NewItem {
+                hash: "0000",
+                name: "keen",
+            })
+            .run(ctx)?;
+            println!("created item: {:?}", item);
+            let edit_item = Item {
+                name: "KeenS".to_string(),
+                ..item
+            };
+            let res = item::update(edit_item).run(ctx)?;
+            match res {
+                None => {
+                    println!("item not found");
+                    return Ok(());
+                }
+                Some(()) => (),
+            };
+            let updated_item = match item::find(item.id).run(ctx)? {
+                None => {
+                    println!("item not found");
+                    return Ok(());
+                }
+                Some(u) => u,
+            };
+
+            println!("updated item: {:?}", updated_item);
+            match item::delete(updated_item.id).run(ctx)? {
+                None => {
+                    println!("item not found");
+                }
+                Some(()) => (),
+            };
+            Ok(())
+        });
+        transaction_diesel_mysql::run(&conn, tx).unwrap()
     }
 }
