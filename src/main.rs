8abef4134eddb205038e4342beeae8549096b6d9
Akira Kano <a.kano1101@gmail.com>
2022-01-10 18:50:51 +0900

    🆙iced導入前時点

--- src/main.rs
+++ src/main.rs
@@ -80,9 +80,11 @@ async fn main() -> AmazonBrowserResult<()> {
     let logs_count = db_logs.len();
     println!("取扱件数は{}件です。", logs_count);
 
-    db_logs.iter().for_each(|db_log| {
-        println!("{:?}", db_log);
-    });
+    // iced
+
+    // db_logs.iter().for_each(|db_log| {
+    //     println!("{:?}", db_log);
+    // });
 
     Ok(())
 }
